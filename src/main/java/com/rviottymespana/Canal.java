a96ac2fd0de63a11e87f9af8de4ec177b36910e8
Viotty Remi <remi.viotty@etudiant.univ-rennes1.fr>
2021-01-26 12:58:46 +0100

    small refactoring without unblocking the Future.get calls

--- src/main/java/com/rviottymespana/Canal.java
+++ src/main/java/com/rviottymespana/Canal.java
@@ -4,8 +4,6 @@ import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-//sera public class Canal implements ObserverDeCapteurAsync, CapteurAsync{
-//joue le role de proxy entre afficheur et capteur, en ralentissant la diffusion des messages
 public class Canal implements ObserverDeCapteurAsync, CapteurAsync{
 
     ScheduledExecutorService scheduler;
@@ -14,10 +12,8 @@ public class Canal implements ObserverDeCapteurAsync, CapteurAsync{
 
     public Canal(ScheduledExecutorService scheduler, Afficheur afficheur) {
         this.scheduler = scheduler;
-        attach(afficheur);
+        this.afficheur = afficheur;
     }
-
-
 
     @Override
     public Future<?> update(Capteur subject) {
@@ -30,30 +26,5 @@ public class Canal implements ObserverDeCapteurAsync, CapteurAsync{
     public Future<Integer> getValue() {
         GetValue getValue = new GetValue(this.subject, this);
         return scheduler.schedule(getValue, (int)(Math.random() * 2000), TimeUnit.MILLISECONDS);
-    }
-
-    // doit être appelé par un ScheduleExecutorService
-    @Override
-    public void tick() {
-    }
-
-    @Override
-    public void lock() {
-        // empeche de modifier la valeur
-    }
-
-    @Override
-    public void unlock() {
-        // lance l'incrémentation
-    }
-
-    @Override
-    public void attach(ObserverDeCapteur o) {
-        afficheur = o;
-    }
-
-    @Override
-    public void detach(ObserverDeCapteur o) {
-
     }
 }
--- src/main/java/com/rviottymespana/CapteurAsync.java
+++ src/main/java/com/rviottymespana/CapteurAsync.java
@@ -4,9 +4,4 @@ import java.util.concurrent.Future;
 
 public interface CapteurAsync {
     Future<Integer> getValue();
-    void tick();
-    void lock();
-    void unlock();
-    void attach(ObserverDeCapteur o);
-    void detach(ObserverDeCapteur o);
 }
